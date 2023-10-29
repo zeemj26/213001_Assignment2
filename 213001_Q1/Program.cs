6e0da1aacbc6201aa5d271b90ac932b4630cefd9
unknown <zee.mj26@hotmail.com>
2023-10-29 11:17:20 +0500

    My Lab Assignment

---
+++ 213001_Q1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+// a) Base class Telephone
+abstract class Telephone
+{
+    protected string phonetype;
+
+    // Abstract method Ring
+    public abstract void Ring();
+
+    // Constructor
+    public Telephone(string type)
+    {
+        phonetype = type;
+    }
+}
+
+// Derived class ElectronicPhone
+class ElectronicPhone : Telephone
+{
+    public ElectronicPhone() : base("Digital") { }
+
+    // Implementing the abstract method Ring
+    public override void Ring()
+    {
+        Console.WriteLine($"Ringing the {phonetype} phone.");
+    }
+
+    // Additional method for demonstration
+    public void Run()
+    {
+        Ring(); // Calls the overridden Ring method
+    }
+}
+
+// Polymorphic method demonstration
+class DigitalPhone : Telephone
+{
+    public DigitalPhone() : base("Digital") { }
+
+    public override void Ring()
+    {
+        Console.WriteLine($"Digital phone is ringing.");
+    }
+}
+
+class TalkingPhone : Telephone
+{
+    public TalkingPhone() : base("Analog") { }
+
+    public override void Ring()
+    {
+        Console.WriteLine($"Analog phone is ringing.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an ElectronicPhone and call its Ring method
+        ElectronicPhone electronicPhone = new ElectronicPhone();
+        electronicPhone.Run();
+
+        // Demonstrate polymorphism
+        Telephone[] phones = new Telephone[]
+        {
+            new ElectronicPhone(),
+            new DigitalPhone(),
+            new TalkingPhone()
+        };
+
+        foreach (Telephone phone in phones)
+        {
+            phone.Ring(); // Calls the appropriate Ring method based on the object's type
+        }
+    }
+}
---
+++ 213001_Q2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ListArrayListDemo
+{
+    public partial class Form1 : Form
+    {
+        private List<string> itemList = new List<string>();
+        private ArrayList arrayList = new ArrayList();
+
+        // Define your controls
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button addButton;
+        private Button removeButton;
+        private Button clearButton;
+        private Button addArrayListButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Add button click event
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                itemList.Add(item);
+                listBox1.Items.Add(item);
+                textBox1.Clear();
+            }
+        }
+
+        // Remove button click event
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                int selectedIndex = listBox1.SelectedIndex;
+                itemList.RemoveAt(selectedIndex);
+                listBox1.Items.RemoveAt(selectedIndex);
+            }
+        }
+
+        // Clear button click event
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            itemList.Clear();
+            listBox1.Items.Clear();
+        }
+
+        // AddArrayList button click event
+        private void addArrayListButton_Click(object sender, EventArgs e)
+        {
+            string item = textBox1.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                arrayList.Add(item);
+                textBox1.Clear();
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            // Initialize and set up your form and controls here.
+            // You can do this in the Form Designer in Visual Studio.
+        }
+    }
+}
